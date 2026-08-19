be07087e9467580f74beb5611513aa7ad1e298f1
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-08-19 17:37:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = 'jarascent-v1';
+const CACHE_NAME = 'jarascent-v2';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './js/rocket_game.js',
-  './js/rocket_engine.js',
-  './js/physics_core.js',
-  './js/rocket_builder.js',
-  './js/rockets_data.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
-  'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/controls/OrbitControls.js'
+  './JarAscenticon-192.png',
+  './JarAscenticon-512.png',
+  './js/rocket_game.js'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).catch(err => console.warn('Cache warning:', err))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
